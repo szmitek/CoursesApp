253e63a8f844ac3dbf7d025016580b372608ebb7
Szmitek <kamilschmidt06@gmail.com>
2023-06-09 17:10:20 +0200

    :construction:  Update to video stream

--- frontend/src/components/SingleCourse.js
+++ frontend/src/components/SingleCourse.js
@@ -7,6 +7,7 @@ const SingleCourse = ({ match }) => {
     const courseId = match.params.id;
     const [course, setCourse] = useState(null);
     const videoRef = useRef(null);
+    const [currentVideo, setCurrentVideo] = useState(null);
     let hls = null;
 
     useEffect(() => {
@@ -28,8 +29,23 @@ const SingleCourse = ({ match }) => {
         }
     };
 
+    const handleVideoClick = (videoUrl, index) => {
+        document.getElementById("url").value = videoUrl;
+        playVideo(index);
+    };
+
+    const playVideo = (index) => {
+        const streamBtn = document.getElementById("streamBtn");
+        if (streamBtn) {
+            setCurrentVideo(index);
+            streamBtn.click();
+        }
+    };
+
+
     useEffect(() => {
         if (Hls.isSupported()) {
+            // HLS is supported, create a new Hls instance and configure it
             const hlsjsConfig = {
                 maxBufferSize: 0,
                 maxBufferLength: 30,
@@ -37,14 +53,17 @@ const SingleCourse = ({ match }) => {
             };
             hls = new Hls(hlsjsConfig);
             hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                // Once the manifest is parsed, start playing the video
                 videoRef.current.play();
             });
         }
 
+        // Regular expression pattern for extracting video IDs from the URL
         const rgx = /([a-z0-9]{10})(:?\/|$)/g;
 
         const stream = async () => {
             if (hls === null) {
+                // HLS is not supported, show an alert message
                 alert("HLS not supported, please use a modern browser such as Chrome");
                 return;
             }
@@ -58,10 +77,12 @@ const SingleCourse = ({ match }) => {
             }
 
             if (ids.length < 1) {
+                // No valid video IDs found in the URL, show an alert message
                 alert("Invalid URL");
                 return;
             }
 
+            // Extract the video ID from the URL
             const videoId = rawUrl.includes("browse3") ? ids[0] : ids[ids.length - 1];
             let statusLabel = document.getElementById("status");
 
@@ -72,12 +93,14 @@ const SingleCourse = ({ match }) => {
 
             for (let i = 300; i <= 1000; i++) {
                 if (i === 1000) {
+                    // Unable to find the last part, show an alert message
                     alert("Error finding the last part");
                     return;
                 }
 
                 if (i === 0) i = 1;
 
+                // Construct the URL for the video part
                 const url = `https://d13z5uuzt1wkbz.cloudfront.net/${videoId}/HIDDEN4500-${String(i).padStart(5, "0")}.ts`;
                 console.log(`Testing ${url}`);
                 statusLabel.innerText = `Looking for the final part; Testing ${i}...`;
@@ -85,6 +108,7 @@ const SingleCourse = ({ match }) => {
                     const resp = await fetch(url, { method: 'HEAD' });
                     if (resp.status === 403) {
                         if (i >= 50 && i % 50 === 0 && jump) {
+                            // Adjust the last part to the previous multiple of 50
                             last = i;
                             jump = true;
                             i -= 51;
@@ -96,6 +120,7 @@ const SingleCourse = ({ match }) => {
                     last = i;
                     jump = false;
                 } catch (e) {
+                    // Fetch failed, show an alert message
                     alert("Fetch failed, please install a Cross-Origin disabler extension for your browser or check your internet connectivity.");
                     return;
                 }
@@ -105,11 +130,13 @@ const SingleCourse = ({ match }) => {
 
             let data = "#EXTM3U\n#EXT-X-PLAYLIST-TYPE:VOD\n#EXT-X-TARGETDURATION:10";
             for (let i = 0; i <= last; i++) {
+                // Generate the HLS playlist data
                 data += `#EXTINF:10,\nhttps://d13z5uuzt1wkbz.cloudfront.net/${videoId}/HIDDEN4500-${String(i).padStart(5, "0")}.ts\n`
             }
 
             console.log(data);
 
+            // Load the HLS playlist data and attach it to the video element
             hls.loadSource("data:application/x-mpegURL;base64," + btoa(data));
             hls.attachMedia(videoRef.current);
         };
@@ -128,7 +155,11 @@ const SingleCourse = ({ match }) => {
                 streamBtn.removeEventListener("click", stream);
             }
         };
-    }, []);
+    }, [currentVideo]);
+    const markAsWatched = (index) => {
+        // Set the current video as watched
+        setCurrentVideo(index);
+    };
 
     if (!course) {
         return <div>Loading...</div>;
@@ -139,7 +170,7 @@ const SingleCourse = ({ match }) => {
             <div className="video-container">
                 <label>URL:</label>
                 <input style={{ width: "300px" }} id="url" placeholder="https://www.skill-capped.com/lol/commentaries/p1qcnwqt75" />
-                <button style={{ width: "80px" }} id="streamBtn">Stream</button>
+                <button style={{ width: "80px", display: "none" }} id="streamBtn">Stream</button>
                 <label style={{ display: "block" }} id="status"></label>
                 <video
                     className="video"
@@ -154,20 +185,24 @@ const SingleCourse = ({ match }) => {
                 />
             </div>
             <div className="content-container">
-            <div className="image-container">
-                <h2>{course.title}</h2>
-                <img src={course.imageURL} alt={course.title} />
-            </div>
-            <div className="playlist-container">
-                <h3>Playlist:</h3>
-                <ul className="playlist">
-                    {course.videos.map((video, index) => (
-                        <li key={index}>
-                            Title: {video.Title}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                <div className="image-container">
+                    <h2>{course.title}</h2>
+                    <img src={course.imageURL} alt={course.title} />
+                </div>
+                <div className="playlist-container">
+                    <h3>Playlist:</h3>
+                    <ul className="playlist">
+                        {course.videos.map((video, index) => (
+                            <li key={index}>
+                                <button onClick={() => handleVideoClick(video.URL, index)}>
+                                    {/* Display check mark if the video has been watched */}
+                                    {index <= currentVideo && <span className="check-mark">&#10003;</span>}
+                                    Title: {video.Title}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
