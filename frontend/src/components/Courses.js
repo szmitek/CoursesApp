1f220b5b280c1d1e53b90bc7d2f076fa07a57781
Szmitek <kamilschmidt06@gmail.com>
2023-06-09 19:14:42 +0200

    :construction:  Added filters by position

--- frontend/src/components/Courses.js
+++ frontend/src/components/Courses.js
@@ -4,6 +4,7 @@ import './styles/CoursesPage.css';
 
 const CoursesPage = () => {
     const [courses, setCourses] = useState([]);
+    const [selectedPosition, setSelectedPosition] = useState('all');
 
     useEffect(() => {
         fetchCourses();
@@ -19,13 +20,30 @@ const CoursesPage = () => {
         }
     };
 
+    const handlePositionChange = (event) => {
+        setSelectedPosition(event.target.value);
+    };
+
+    const filteredCourses = selectedPosition === 'all' ? courses : courses.filter((course) => course.position === selectedPosition);
+
     console.log('Courses:', courses); // Check the value of the courses array
 
     return (
         <div className="courses-container">
             <h2>Courses Page</h2>
+            <div className="position-filter">
+                <label htmlFor="position-select">Filter by Position:</label>
+                <select id="position-select" value={selectedPosition} onChange={handlePositionChange}>
+                    <option value="all">All</option>
+                    <option value="mid">Mid</option>
+                    <option value="top">Top</option>
+                    <option value="bot">Bot</option>
+                    <option value="support">Support</option>
+                    <option value="jungle">Jungle</option>
+                </select>
+            </div>
             <div className="courses-grid">
-                {courses.map((course) => (
+                {filteredCourses.map((course) => (
                     <div key={course.id} className="course-box">
                         <div className="course-image">
                             <div className="position-tag">{course.position}</div>
